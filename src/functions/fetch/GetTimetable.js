3ae8e43febc3fe8491b8186a3747bc00fba7b426
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2022-12-19 19:36:31 +0100

    Ajout d'un cache

--- src/functions/fetch/GetTimetable.js
+++ src/functions/fetch/GetTimetable.js
@@ -28,31 +28,55 @@ function getPronoteTimetable(date) {
     // construct url (date is a TEST date)
     let URL = `${API}/timetable?dateString=${dayString}&token=${token}`;
 
-    // get timetable from API
-    return axios.get(URL)
-    .then((response) => {
-        // get timetable
-        let timetable = response.data;
-
-        // construct timetable
-        timetable = constructPronoteTimetable(timetable);
-
-        // return timetable
-        return timetable;
-    })
-    .catch((error) => {
-        if (error.response) {
-            // check if "notfound" or "expired"
-            if (error.response.data == "notfound") {
-                // get new token
-                GetToken();
-            }
-            else if (error.response.data == "expired") {
-                // get new token
-                GetToken();
-            }
-        }
+    // check if timetable is cached
+    let cacheSearch = JSON.parse(localStorage.getItem('TimetableCache')) || [];
+    cacheSearch = cacheSearch.filter((element) => {
+        return element.date == dayString && element.token == token;
     });
+    if (cacheSearch.length > 0) {
+        // return cached timetable in promise
+        return new Promise((resolve, reject) => {
+            let timetable = JSON.parse(cacheSearch[0].timetable);
+            resolve(constructPronoteTimetable(timetable));
+        });
+    }
+    else {
+        // get timetable from API
+        return axios.get(URL)
+        .then((response) => {
+            // get timetable
+            let timetable = response.data;
+
+            // construct timetable
+            timetable = constructPronoteTimetable(timetable);
+
+            // cache response
+            let cache = JSON.parse(localStorage.getItem('TimetableCache')) || [];
+            let cacheElement = {
+                date: dayString,
+                token: token,
+                timetable: JSON.stringify(response.data)
+            };
+            cache.push(cacheElement);
+            localStorage.setItem('TimetableCache', JSON.stringify(cache));
+
+            // return timetable
+            return timetable;
+        })
+        .catch((error) => {
+            if (error.response) {
+                // check if "notfound" or "expired"
+                if (error.response.data == "notfound") {
+                    // get new token
+                    GetToken();
+                }
+                else if (error.response.data == "expired") {
+                    // get new token
+                    GetToken();
+                }
+            }
+        });
+    }
 }
 
 // pronote : construct timetable
