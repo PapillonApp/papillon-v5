89a42efbd72c0b2d4c67eb0667ee6c1b1e4293fc
Luc'HACK <39491773+lucas-luchack@users.noreply.github.com>
2023-01-09 11:43:28 +0100

    Add mounted()

--- src/views/MenusView.vue
+++ src/views/MenusView.vue
@@ -4,6 +4,9 @@
 
     import { Capacitor } from '@capacitor/core';
 
+	import { Swiper, SwiperSlide } from 'swiper/vue';
+	import 'swiper/css';
+
     import GetToken from '@/functions/login/GetToken.js';
     import GetMenus from '@/functions/fetch/GetMenus.js';
 
@@ -26,8 +29,62 @@
 
         },
         mounted() {
+			// on rnChanged, update rnButtonString
+			document.addEventListener('rnChanged', (e) => {
+				this.rnButtonString = this.createDateString(e.detail);
+			});
 
-            return false;
+			// get timetable data
+			this.getTimetables();
+
+			// on rnChanged, get new timetable data
+			document.addEventListener('rnChanged', (e) => {
+				this.getTimetables();
+			});
+
+			// on token changed, get new timetable data
+			document.addEventListener('tokenUpdated', (e) => {
+				this.getTimetables();
+			});
+
+			// detect swiper slide change
+			let swiper = this.$refs.swiper.$el.swiper;
+
+			swiper.on('touchEnd', () => {
+				setTimeout(() => {
+					// get new rn
+					// check if swiper is on yesterday
+					if(swiper.activeIndex == 0) {
+						let newRn = new Date(this.$rn);
+						newRn.setDate(newRn.getDate() - 1);
+
+						this.$rn = newRn;
+						this.rnCalendarString = this.$rn.toISOString().split('T')[0];
+
+						// emit event
+						document.dispatchEvent(new CustomEvent('rnChanged', { detail: this.$rn }));
+
+						// reset swiper
+						this.shouldResetSwiper = true;
+					}
+
+					// check if swiper is on tomorrow
+					if(swiper.activeIndex == 2) {
+						// add 1 day to rn
+						let newRn = new Date(this.$rn);
+						newRn.setDate(newRn.getDate() + 1);
+
+						this.$rn = newRn;
+						this.rnCalendarString = this.$rn.toISOString().split('T')[0];
+
+						// emit event
+						document.dispatchEvent(new CustomEvent('rnChanged', { detail: this.$rn }));
+
+						// reset swiper
+						this.shouldResetSwiper = true;
+					}
+				}, 200);
+			});
         }
     });
 </script>
@@ -42,10 +99,22 @@
           </ion-buttons>
 
           <ion-title mode="md">Menus</ion-title>
+
+		  <ion-buttons slot="end">
+				<ion-button mode="md" color="dark" id="rnPickerModalButton" @click="openRnModal">
+					<span class="material-symbols-outlined mdls" slot="start">calendar_month</span>
+
+					<p>{{ rnButtonString }}</p>
+				</ion-button>
+			</ion-buttons>
         </IonToolbar>
       </IonHeader>
       
       <ion-content :fullscreen="true">
+		<ion-refresher slot="fixed" @ionRefresh="handleRefresh($event)">
+			<ion-refresher-content></ion-refresher-content>
+		</ion-refresher>
+		
         <IonHeader collapse="condense">
             <IonToolbar>
                 <ion-title size="large">Menus</ion-title>
