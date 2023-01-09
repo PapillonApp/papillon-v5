2cfcee067c9c9ea8e9e71be027d91615e0528f0c
Luc'HACK <39491773+lucas-luchack@users.noreply.github.com>
2023-01-09 13:00:51 +0100

    Add Menu methods on the page Need to do the style

--- src/views/MenusView.vue
+++ src/views/MenusView.vue
@@ -19,14 +19,106 @@
             IonTitle,
             IonMenuButton,
             IonPage,
+			IonDatetime,
+			Swiper,
+			SwiperSlide,
+			IonRefresher,
+			IonRefresherContent,
+			IonSpinner,
         },
         data() {
             return { 
-
+				rnButtonString: this.createDateString(this.$rn),
+				loadedrnButtonString: this.createDateString(this.$rn),
+				rnCalendarString: this.$rn.toISOString().split('T')[0],
+				menu: [],
+				yesterday: [],
+				tomorrow: [],
+				shouldResetSwiper: false,
+				dontRetryCheck: false,
             }
         },
         methods: {
+			createDateString(date) {
+				let dateObject = new Date(date);
 
+				// return string like "1 jan."
+				return `${dateObject.getDate()} ${dateObject.toLocaleString('default', { month: 'short' })}`;
+			},
+			rnInputChanged() {
+				// get new date from rnInput
+				let newDate = new Date(this.$refs.rnInput.$el.value);
+
+				// update rn
+				this.$rn = newDate;
+
+				// emit event
+				document.dispatchEvent(new CustomEvent('rnChanged', { detail: newDate }));
+			},
+			confirmRnInput() {
+				this.$refs.rnPickerModal.$el.dismiss();
+			},
+			openRnModal() {
+				this.$refs.rnPickerModal.$el.present();
+			},
+			handleRefresh(event) {
+				// get new menu data
+				this.getMenus(true);
+
+				// stop refresh when this.menu is updated
+				this.$watch('menu', () => {
+					setTimeout(() => {
+						event.target.complete();
+					}, 200);
+				});
+			},
+			getMenus(force) {
+				// reset swiper and show loading spinner
+				if(this.shouldResetSwiper) {
+					this.$refs.swiper.$el.swiper.slideTo(1, 0);
+					this.shouldResetSwiper = false;
+
+					this.menu = [];
+					this.yesterday = [];
+					this.tomorrow = [];
+
+					this.menu.loading = true;
+					this.yesterday.loading = true;
+					this.tomorrow.loading = true;
+
+					this.menu.error = "STILL_LOADING";
+					this.yesterday.error = "STILL_LOADING";
+					this.tomorrow.error = "STILL_LOADING";
+				}
+
+				// get menus for rn
+				GetMenus(this.$rn, force).then((menu) => {
+					this.menu = menu
+					this.loadedrnButtonString = this.createDateString(this.$rn);
+					this.menu.loading = false;
+
+					this.dontRetryCheck = true;
+
+					setTimeout(() => {
+						this.dontRetryCheck = false;
+					}, 200);
+				});
+
+				// get menu for yesterday
+				let yesterdayRN = new Date(this.$rn) - 86400000;
+				GetMenus(yesterdayRN, force).then((menu) => {
+					this.yesterday = menu
+					this.yesterday.loading = false
+				});
+
+				// get menu for tomorrow
+				let tomorrowRN = new Date(this.$rn);
+				tomorrowRN.setDate(tomorrowRN.getDate() + 1);
+				GetMenus(tomorrowRN, force).then((menu) => {
+					this.tomorrow = menu
+					this.tomorrow.loading = false
+				});
+			},
         },
         mounted() {
 			// on rnChanged, update rnButtonString
@@ -35,16 +127,16 @@
 			});
 
 			// get timetable data
-			this.getTimetables();
+			this.getMenus();
 
 			// on rnChanged, get new timetable data
 			document.addEventListener('rnChanged', (e) => {
-				this.getTimetables();
+				this.getMenus();
 			});
 
 			// on token changed, get new timetable data
 			document.addEventListener('tokenUpdated', (e) => {
-				this.getTimetables();
+				this.getMenus();
 			});
 
 			// detect swiper slide change
