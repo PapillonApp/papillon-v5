c99ae74a913e1a0a399a1cf44e8b71f8c8d1d42f
andronedev <andronedev@gmail.com>
2023-01-10 11:46:34 +0100

    make robust

--- src/functions/fetch/GetContributors.js
+++ src/functions/fetch/GetContributors.js
@@ -1,17 +1,28 @@
 // modules
 import axios from 'axios';
 
-const github_api_url = "https://api.github.com/repos/PapillonApp/papillon-v4"
+const github_api_url = "https://api.github.com/repos/PapillonApp/papillon"
 // main function
 async function getContributors(limit = 100) {
     return new Promise((resolve, reject) => {
+        
+        // check if limit is a number
+        if (typeof limit !== 'number') {
+            reject('Limit must be a number');
+        }
+
+        // check if limit is higher than 0
+        if (limit < 1) {
+            reject('Limit must be higher than 0');
+        }
+
         axios.get(github_api_url + "/contributors")
         .then((response) => {
             // return limit 
             resolve(response.data.slice(0, limit));
         })
         .catch((error) => {
-            reject(error);
+            reject('Problem with fetching contributors (' + error + ')');
         })
     })
 }
