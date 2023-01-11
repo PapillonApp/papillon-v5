8a1b51416b8ae6904f684d2082910c83c8426647
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2023-01-11 20:57:17 +0100

    Use random color instead of course colors which looks bad almost everytime

--- src/functions/utils/subjectColor.js
+++ src/functions/utils/subjectColor.js
@@ -1,61 +1,37 @@
-/* 
-make an array of colors from
-background: #00934F;
-background: #12957D;
-background: #1978AD;
-background: #7937CE;
-background: #931BB1;
-background: #AA611E;
-background: #AD125D;
-background: #BE381A;
-background: #C12C2C;
-*/
-
-const baseColors = [ '#00934F', '#12957D', '#1978AD', '#7937CE', '#931BB1', '#AA611E', '#AD125D', '#BE381A', '#C12C2C', "#BB7B1B", '#AA831E' ];
-
-function hexToRgb(hex) {
-	console.log(hex);
-
-	var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
-	hex = hex.replace(shorthandRegex, function(m, r, g, b) {
-		return r + r + g + g + b + b;
-	});
-	
-	var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-	return result ? {
-		r: parseInt(result[1], 16),
-		g: parseInt(result[2], 16),
-		b: parseInt(result[3], 16)
-	} : null;
-}
-
-function distance(a, b) {
-	return Math.sqrt(Math.pow(a.r - b.r, 2) + Math.pow(a.g - b.g, 2) + Math.pow(a.b - b.b, 2));
-}
-
-function nearestColor(colorHex){
-	console.log(colorHex);
-	var lowest = Number.POSITIVE_INFINITY;
-	var tmp;
-	let index = 0;
-
-	baseColors.forEach( (el, i) => {
-		tmp = distance(hexToRgb(colorHex), hexToRgb(el))
-		if (tmp < lowest) {
-			lowest = tmp;
-			index = i;
-		}
-	})
-
-	return baseColors[index];
-}
-
+/* give 50 random colors of all hues but with enough contrast with white text */
 function getRandomColor() {
 	var color = '#';
 	for (var i = 0; i < 6; i++) {
 		color += Math.floor(Math.random() * 10);
 	}
 	return color;
+}
+
+function LightenDarkenColor(col,amt) {
+    var usePound = false;
+    if ( col[0] == "#" ) {
+        col = col.slice(1);
+        usePound = true;
+    }
+
+    var num = parseInt(col,16);
+
+    var r = (num >> 16) + amt;
+
+    if ( r > 255 ) r = 255;
+    else if  (r < 0) r = 0;
+
+    var b = ((num >> 8) & 0x00FF) + amt;
+
+    if ( b > 255 ) b = 255;
+    else if  (b < 0) b = 0;
+    
+    var g = (num & 0x0000FF) + amt;
+
+    if ( g > 255 ) g = 255;
+    else if  ( g < 0 ) g = 0;
+
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
 }
 
 function lightenColor(color, percent) {
@@ -78,7 +54,8 @@ function setSubjectColor(subjectName, color, force=false) {
 		color = '#' + color;
 	}
 
-	color = nearestColor(color);
+	// make color a random element from baseColors (avoid duplicates)
+	color = getRandomColor()
 
 	if (subjectColors[subjectName] && !force) {
 		return subjectColors[subjectName];
@@ -113,4 +90,4 @@ function getSubjectColor(subjectName, color) {
 	return setSubjectColor(subjectName, color);
 }
 
-export default { setSubjectColor, getSubjectColor, getRandomColor, lightenColor, darkenHexColor, nearestColor };
+export default { setSubjectColor, getSubjectColor, getRandomColor, lightenColor, darkenHexColor };
