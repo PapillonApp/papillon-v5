2d64f418feaf7a45e66a0a0e86e5d540027acc7d
Luc'HACK <espiet.l@valbion.com>
2023-01-19 22:55:55 +0100

    🚨 POUR VINCENT - NE PAS BUILD AVEC CECI

--- src/functions/fetch/GetAbsences.js
+++ src/functions/fetch/GetAbsences.js
@@ -35,6 +35,42 @@ async function getPronoteAbsences(forceReload) {
 		return axios.get(URL)
 		.then((response) => {
 			absences = response.data;
+
+			// FIXME : NEED TO BE DELETED AFTER DESIGN MADE
+			absences = [
+				{
+					"id": "1DB8FEAD2862E",
+					"from": "2022-11-22 10:00",
+					"to": "2022-11-22 12:30",
+					"justified": true,
+					"hours": "2h30",
+					"reasons": [
+						"RDV MEDICAL EXTERIEUR"
+					]
+				},
+				{
+					"id": "10E95E4D2866F",
+					"from": "2023-04-03 10:00",
+					"to": "2023-04-03 17:30",
+					"justified": false,
+					"hours": "5h30",
+					"reasons": [
+						"RDV MEDICAL EXTERIEUR"
+					]
+				},
+				{
+					"id": "165B7E4D2863B",
+					"from": "2023-05-05 09:00",
+					"to": "2023-05-05 11:00",
+					"justified": true,
+					"hours": "2h00",
+					"reasons": [
+						"RDV MEDICAL EXTERIEUR"
+					]
+				}
+			]
+
+
 			absences = constructPronoteAbsences(absences);
 			
             let today = new Date();
