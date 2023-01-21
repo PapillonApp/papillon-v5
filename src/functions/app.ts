3dd7d9d981bf2b5cef8d53fb0ae2087d602080a3
Vince Linise <32978709+ecnivtwelve@users.noreply.github.com>
2023-01-21 15:09:08 +0100

    🐛 Ajout d'une vérification constante de la status bar

--- src/functions/app.ts
+++ src/functions/app.ts
@@ -11,31 +11,36 @@ let isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: 
 import { StatusBar, Style } from '@capacitor/status-bar';
 
 function checkDarkMode() {
+  // vérication du mode sombre depuis le CSS
   isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 }
 
 function setStatusBarStyle() {
   if (isDarkMode) {
+    // status bar en mode sombre
     StatusBar.setBackgroundColor({color: "#1C1B1F"});
     StatusBar.setStyle({style: Style.Dark})
   }
   else {
+    // status bar en mode clair
     StatusBar.setBackgroundColor({color: "#ffffff"});
     StatusBar.setStyle({style: Style.Light})
   }
 }
 
-// Navigation bar
+// Navigation bar (Android only)
 import { NavigationBar } from '@hugotomazi/capacitor-navigation-bar';
 
 function setNavigationBarStyle() {
   if (isDarkMode) {
+    // navigation bar en mode sombre
     NavigationBar.setColor({
         color: "#1C1B1F",
         darkButtons: false
     });
   }
   else {
+    // navigation bar en mode clair
     NavigationBar.setColor({
         color: "#ffffff",
         darkButtons: true
@@ -43,17 +48,23 @@ function setNavigationBarStyle() {
   }
 }
 
-// Constantly check for dark mode
-setTimeout(() => {
-  checkDarkMode();
-  setStatusBarStyle();
-  if (Capacitor.getPlatform() === 'android') {
-    setNavigationBarStyle();
+function setDarkMode() {
+  // si nous sommes sur mobile
+  if (Capacitor.getPlatform() !== 'web') {
+    // vérification du mode sombre
+    checkDarkMode();
+    // changement du style de la status bar
+    setStatusBarStyle();
+    // changement du style de la navigation bar (Android only)
+    if (Capacitor.getPlatform() === 'android') {
+      // changement du style de la navigation bar
+      setNavigationBarStyle();
+    }
   }
-}, 1000);
-
-// Set status bar and navigation bar style
-setStatusBarStyle();
-if (Capacitor.getPlatform() === 'android') {
-  setNavigationBarStyle();
 }
+
+// Constantly check for dark mode
+setInterval(setDarkMode, 1000);
+
+// première vérification du mode sombre
+setDarkMode();
