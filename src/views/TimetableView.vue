6e46229a931bdee3fb409e86a6b4d5e895c1a73d
Luc'HACK <espiet.l@valbion.com>
2023-02-04 01:07:23 +0100

    🎨 Change modal size

--- src/views/TimetableView.vue
+++ src/views/TimetableView.vue
@@ -670,7 +670,7 @@
         </IonModal>
 
 
-        <IonModal :is-open="rnPickerModalOpen" ref="rnPickerModal" class="datetimeModal" @didDismiss="changernPickerModalOpen(false)" :keep-contents-mounted="true" :initial-breakpoint="0.55" :breakpoints="[0, 0.55, 1]">
+        <IonModal :is-open="rnPickerModalOpen" ref="rnPickerModal" class="datetimeModal" @didDismiss="changernPickerModalOpen(false)" :keep-contents-mounted="true" :initial-breakpoint="0.5" :breakpoints="[0, 0.5]">
           <IonHeader>
             <IonToolbar>
               <ion-title>Sélection de la date</ion-title>
