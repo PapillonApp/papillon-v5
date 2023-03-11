b85f50079f83eb6896bda11f9236b8acf4452618
Cuauh <cuauh@cuauh.fr>
2023-03-11 18:55:36 +0100

    � Add icons for custom courses

--- src/views/TimetableView.vue
+++ src/views/TimetableView.vue
@@ -706,20 +706,30 @@
             <ion-content>
                 <ion-list inset>
                     <ion-item>
-                        <ion-input type="text" name="CourseTitle" ref="newCoursNameRef" placeholder="Nom du cours"></ion-input>
+                        <span class="material-symbols-outlined mdls" slot="start" style="margin-right:5px">Title</span>
+                        <ion-label>
+                            <ion-input type="text" name="CourseTitle" ref="newCoursNameRef" placeholder="Nom du cours"></ion-input>
+                        </ion-label>
                     </ion-item>
 
                     <ion-item>
-                        <ion-input type="text" name="Place" ref="newCoursRoomRef" placeholder="Lieu"></ion-input>
+                        <span class="material-symbols-outlined mdls" slot="start" style="margin-right:5px">pin_drop</span>
+                        <ion-label>
+                            <ion-input type="text" name="Place" ref="newCoursRoomRef" placeholder="Lieu"></ion-input>
+                        </ion-label>
                     </ion-item>
 
                     <ion-item>
-                        <ion-input type="text" name="Person" ref="newCoursTeacherRef" placeholder="Professeur"></ion-input>
+                        <span class="material-symbols-outlined mdls" slot="start" style="margin-right:5px">Person</span>
+                        <ion-label>
+                            <ion-input type="text" name="Person" ref="newCoursTeacherRef" placeholder="Professeur"></ion-input>
+                        </ion-label>
                     </ion-item>
                 </ion-list>
 
                 <ion-list inset>
                     <ion-item class="input">
+                        <span class="material-symbols-outlined mdls" slot="start" style="margin-right:15px">schedule</span>
                         <ion-label>Heure de début</ion-label>
                         <div class="timeInput" slot="end">
                             <ion-input ref="newCoursStartRef" class="timeInInput" name="Start" type="time" value="12:30"></ion-input>
@@ -727,6 +737,7 @@
                     </ion-item>
 
                     <ion-item class="input">
+                        <span class="material-symbols-outlined mdls" slot="start" style="margin-right:15px">schedule</span>
                         <ion-label>Heure de fin</ion-label>
                         <div class="timeInput" slot="end">
                             <ion-input class="timeInInput" name="End" ref="newCoursEndRef" type="time" value="13:30"></ion-input>
