2c7711d49fb425054f3a370a41d36b50d0dbfda4
LeMaitre4523 <arm2006gir@gmail.com>
2023-03-12 21:52:30 +0100

    GetRecap - ed

--- src/functions/fetch/GetRecap.js
+++ src/functions/fetch/GetRecap.js
@@ -13,7 +13,7 @@ async function getRecap(force) {
 			// return pronote timetable
 			return getPronoteRecap(force);
 		case "ecoledirecte":
-            return;
+            return getEDRecap(force);
     }
 }
 
@@ -107,6 +107,109 @@ function getPronoteRecap(force) {
 	});
 
 }
+
+
+
+
+
+
+
+// ed : get timetable
+function getEDRecap(force) {
+	// promise
+	return new Promise((resolve, reject) => {
+		// vars
+		let timetable = []
+		let homeworks = []
+		let grades = {
+			full: [],
+			last: []
+		}
+		let news = []
+
+		let requestsDone = 0;
+		
+		// timetable
+		getTimetable(new Date(), force).then((response) => {
+			console.log(response)
+			timetable = response;
+			requestsDone++;
+		});
+
+		// homeworks
+		let today = new Date();
+
+		let endDate = new Date();
+		endDate.setDate(endDate.getDate() + 3);
+
+		getHomeworks(today, endDate, force).then((response) => {
+			homeworks = response;
+			requestsDone++;
+		});
+
+		// grades
+		/*getGrades(force).then((response) => {
+			grades.full = response;
+			requestsDone++;
+
+			// get all grades in all subjects
+			let allGrades = [];
+			for (let i = 0; i < grades.full.marks.length; i++) {
+				for (let j = 0; j < grades.full.marks[i].marks.length; j++) {
+					let newGrade = grades.full.marks[i].marks[j];
+					// add subject data
+					newGrade.subject = {
+						name: grades.full.marks[i].name,
+						color: grades.full.marks[i].color,
+						id: grades.full.marks[i].id
+					};
+
+					allGrades.push(newGrade);
+				}
+			}
+
+			// sort grades by date
+			allGrades.sort((a, b) => {
+				return new Date(b.info.date) - new Date(a.info.date);
+			});
+
+			// get last 5 grades
+			for (let i = 0; i < 5; i++) {
+				if (allGrades[i] != undefined) {
+					grades.last.push(allGrades[i]);
+				}
+			}
+		});
+
+		// news
+		getNews(force).then((response) => {
+			news = response;
+			requestsDone++;
+		});*/
+
+		// wait for all requests to be done
+		let interval = setInterval(() => {
+			if (requestsDone == 4) {
+				clearInterval(interval);
+
+				// return recap
+				resolve({
+					timetable: timetable,
+					homeworks: homeworks,
+					grades: grades,
+					news: news
+				});
+			}
+		});
+
+	});
+
+}
+
+
+
+
+
 
 // export
 export default getRecap
