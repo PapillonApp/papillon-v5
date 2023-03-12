b182ab1cdd764d1252598200090cf238e647f766
LeMaitre4523 <arm2006gir@gmail.com>
2023-03-12 21:17:09 +0100

    edit message code - timetable

--- src/functions/fetch/GetTimetable.js
+++ src/functions/fetch/GetTimetable.js
@@ -244,11 +244,7 @@ function getEDTimetable(date, forceReload) {
         .catch((error) => {
             if (error.response) {
                 // check if "notfound" or "expired"
-                if (error.response.data == "notfound") {
-                    // get new token
-                    GetToken();
-                }
-                else if (error.response.data == "expired") {
+                if (error.response.data.code == 525) {
                     // get new token
                     GetToken();
                 }
