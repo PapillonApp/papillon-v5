e9cdb7827ac15a41339aa8444da6ba1b20b3e145
LeMaitre4523 <arm2006gir@gmail.com>
2023-03-12 21:16:55 +0100

    GetHomeworks - en cours

--- src/functions/fetch/GetHomeworks.js
+++ src/functions/fetch/GetHomeworks.js
@@ -14,7 +14,7 @@ async function getHomeworks(dateFrom, dateTo, forceReload) {
             // return pronote homework
             return getPronoteHomework(dateFrom, dateTo, forceReload);
 		case "ecoledirecte":
-            return;
+            return getEDHomework(dateFrom, dateTo, forceReload)
     }
 }
 
@@ -162,6 +162,209 @@ function constructPronoteHomework(hw) {
     // return courses
     return homeworkArray;
 }
+
+
+
+
+
+
+
+// ed : get homework
+function getEDHomework(dateFrom, dateTo, forceReload) {
+    // gather vars
+    const EDAPI = "https://api.ecoledirecte.com/v3"//app.config.globalProperties.$api;
+    const dayRequest = new Date(dateFrom);
+    const dayRequestTo = new Date(dateTo);
+
+    // get token
+    const token = localStorage.getItem('token');
+    const userID = JSON.parse(localStorage.UserCache).id;
+
+    // get date as YYYY-MM-DD
+    const dayString = dayRequest.toISOString().split('T')[0];
+    const dayStringTo = dayRequestTo.toISOString().split('T')[0];
+
+    // construct url (date is a TEST date)
+    let URL = `${EDAPI}/Eleves/${userID}/cahierdetexte.awp?verbe=get`;
+
+    // check if homework is cached
+    let cacheSearch = JSON.parse(localStorage.getItem('HomeworkCache')) || [];
+    cacheSearch = cacheSearch.filter((element) => {
+        return element.dateFrom == dayString && element.dateTo == dayStringTo;
+    });
+    if (cacheSearch.length > 0 && !forceReload) {
+        // return cached homework in promise
+        return new Promise((resolve) => {
+            let homework = JSON.parse(cacheSearch[0].homework);
+            resolve(constructEDHomework(homework));
+        });
+    }
+    else {
+        // get homework from API
+
+        var requestOptions = {
+            headers: { "Content-Type": "application/x-www-form-urlencoded", "X-Token": token },            
+        };
+        let body = `data={}`
+
+
+        return axios.post(URL, body, requestOptions)
+            .then((response) => {
+                // get homework
+                let homeworks = response.data.data;
+
+                // construct homework
+                homeworks = constructEDHomework(homeworks);
+
+                // cache response
+                let cache = JSON.parse(localStorage.getItem('HomeworkCache')) || [];
+                let cacheElement = {
+                    dateFrom: dayString,
+                    dateTo: dayStringTo,
+                    token: token,
+                    homework: JSON.stringify(response.data.data)
+                };
+                cache.push(cacheElement);
+                localStorage.setItem('HomeworkCache', JSON.stringify(cache));
+
+                // return homeworks
+                return homeworks;
+            })
+            .catch((error) => {
+                if (error.response) {
+                    if (error.response.data.code == 525) {
+                        // get new token
+                        GetToken();
+                    }
+                }
+
+                if(error.code) {
+                    // return empty timetable in promise
+                    return new Promise((reject) => {
+                        reject({
+                            error: error.code
+                        });
+                    });
+                }
+            });
+    }
+}
+
+// ed : construct homework
+function constructEDHomework(hw) {
+    // declaring vars
+    let homeworkArray = [];
+
+    
+    const token = localStorage.getItem('token');
+    const userID = JSON.parse(localStorage.UserCache).id;
+
+    // for each course in homework
+    hw.forEach((date) => {
+        //on obtiens une date avec une liste
+        //2023-03-17
+        hw[date].forEach((homework) => {
+
+            let URL = `https://api.ecoledirecte.com/v3/Eleves/${userID}/cahierdetexte/${date}.awp?verbe=get`;
+            var requestOptions = {
+                headers: { "Content-Type": "application/x-www-form-urlencoded", "X-Token": token },            
+            };
+            let body = `data={}`
+
+
+            axios.post(URL, body, requestOptions)
+            .then((response) => {
+                // get homework
+                let homeworks = response.data.data;
+
+                homeworks.matieres.forEach((hws) => {
+                    
+                    //foreach documents
+                    hws.aFaire.documents.forEach((file) => {
+                        // if no file.name, set it to "Document"
+                        if (!file.libelle) {
+                            file.name = "Document";
+                        }
+                        // if file.url is not a link
+                        /*if (!file.url.startsWith("http")) {
+                            // remove file
+                            homework.files.splice(homework.files.indexOf(file), 1);
+                        }*/
+                    });
+
+                    //homework description
+                    hws.aFaire.contenu = atob(hws.aFaire.contenu)
+                    let homeworkDescription = hws.aFaire.contenu;
+                    /*
+                        Traitement des balises HTML (<strong>, <u>)
+                    
+                    
+                    */
+
+
+                    if (homeworkDescription.length > 80) {
+                        homeworkDescription = homeworkDescription.substring(0, 80);
+                        homeworkDescription = homeworkDescription.substring(0, homeworkDescription.lastIndexOf(" "));
+                        homeworkDescription += "...";
+                    }
+
+
+                    // replace new lines in homework.description with <br/>
+                    hws.aFaire.contenu = hws.aFaire.contenu.replace(/\n/g, "<br/>");
+                    // parse links in homework.description
+                    hws.aFaire.contenu = hws.aFaire.contenu.replace(/(https?:\/\/[^\s]+)/g, '<a href="$1" target="_blank">$1</a>');
+
+
+
+
+                    // construct course
+                    let newHomework = {
+                        data: {
+                            id: hws.id,
+                            date: hws.donneLe.replace(/-/g, "/"),
+                            color: subjectColor.getSubjectColor(hws.matiere, hws.color || "#12d4a6"),
+                            done: hws.effectue,
+                        },
+                        homework: {
+                            subject: hws.matiere,
+                            content: hws.description,
+                            shortContent: homeworkDescription,
+                        },
+                        files: hws.aFaire.documents,
+                    };
+
+                    subjectColor.setSubjectColor(newHomework.homework.subject, newHomework.data.color, true);
+
+                    // push course to courses
+                    homeworkArray.push(newHomework);
+
+                })
+            })
+            .catch((error) => {
+                if (error.response) {
+                    if (error.response.data.code == 525) {
+                        // get new token
+                        GetToken();
+                    }
+                }
+
+                if(error.code) {
+                    // return empty timetable in promise
+                    return new Promise((reject) => {
+                        reject({
+                            error: error.code
+                        });
+                    });
+                }
+            });
+        })
+    });
+
+    // return courses
+    return homeworkArray;
+}
+
+
 
 // export
 export default getHomeworks;
