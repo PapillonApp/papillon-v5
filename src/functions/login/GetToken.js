9707203312d741b23b8dca5cc03ee3c185e06a65
LeGeek01 <timeobailly02@gmail.com>
2023-03-12 18:10:14 +0100

    fix gettoken

--- src/functions/login/GetToken.js
+++ src/functions/login/GetToken.js
@@ -136,7 +136,7 @@ function getEDLogin() {
         let password = loginData.password;
 
         var requestOptions = {
-            headers: { "Content-Type": "application/x-www-form-urlencoded", "Accept": "application/json, text/plain, */*", "X-Token": "", "User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"},            
+            headers: { "Content-Type": "application/x-www-form-urlencoded", "X-Token": ""},            
         };
         let body = `data={
             "uuid": "",
