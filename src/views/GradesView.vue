ad05e5eeb026e86e6ed4389223a6c8e9523baee1
Luc'HACK <espiet.l@valbion.com>
2023-03-18 20:01:26 +0100

    🩹 Fix toolbar

--- src/views/GradesView.vue
+++ src/views/GradesView.vue
@@ -418,8 +418,8 @@
 					</ion-select>
 				</ion-buttons>
 			</IonToolbar>
-			<IonToolbar>
-				<ion-segment v-if="periods.length > 0 && changePeriodSelection" id="segment" :value="current_period.id"
+			<IonToolbar v-if="changePeriodSelection">
+				<ion-segment v-if="periods.length > 0" id="segment" :value="current_period.id"
 				ref="segment" @ionChange="segChange()">
 					<ion-segment-button v-for="(period, i) in periods" :key="i" :value="period.id" :id="period.id">
 						<ion-label>{{period.name}}</ion-label>
