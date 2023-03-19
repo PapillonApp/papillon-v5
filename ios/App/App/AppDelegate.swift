6d34bc07c48877e992377321b7afc96ffee45e19
Vince Linise <vincelinise@gmail.com>
2023-03-19 21:34:26 +0100

    🔨 Changements liés à iOS

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -10,8 +10,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         // [capacitor-background-fetch]
-        let fetchManager = TSBackgroundFetch.sharedInstance();
-        fetchManager?.didFinishLaunching();
         return true
     }
     
