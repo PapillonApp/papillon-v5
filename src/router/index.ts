e4e5d0a8886ae7859165a88cd6443a8caa1c5087
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-04-04 16:38:01 +0200

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
+import type { RouteRecordRaw } from 'vue-router';
 
 type Routes = RouteRecordRaw[]
 
