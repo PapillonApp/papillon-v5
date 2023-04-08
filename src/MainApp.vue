67286b53b129b25df279b051db5744c6b3c580f8
Florian LEFEBVRE <contact@florian-lefebvre.dev>
2023-04-08 14:00:01 +0200

    Refactor MainApp.vue to use the Composition API (#89)

--- src/MainApp.vue
+++ src/MainApp.vue
@@ -1,28 +1,18 @@
 <template>
     <ion-page>
         <!-- main navigation (contains router and menu) -->
-        <ion-nav ref="nav" :root="component" :swipeGesture="true"></ion-nav>
+        <ion-nav ref="nav" :root="App" :swipeGesture="true"></ion-nav>
     </ion-page>
 </template>
   
-<script lang="ts">
-    import { defineComponent } from 'vue';
+<script setup lang="ts">
+    import { ref, onMounted } from 'vue';
     import { IonPage, IonNav } from '@ionic/vue';
     import App from './App.vue';
-  
-    export default defineComponent({
-        components: {
-            IonNav,
-            IonPage,
-        },
-        data() {
-            return {
-                component: App,
-            };
-        },
-        mounted() {
-            // make nav globally available to other components
-            (window as any).nav = this.$refs.nav;
-        },
-    });
+    
+    const nav = ref(null)
+
+    onMounted(() => {
+        (window as any).nav = nav;
+    })
 </script>
