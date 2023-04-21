1138351af2b0e1b80c12cf35e46dadeb558ab115
LeGeek01 <timeobailly02@gmail.com>
2023-04-21 15:01:35 +0200

    création SettingsSetup

---
+++ src/views/settings/SettingsSetup.vue
@@ -0,0 +1,22 @@
+<template>
+    <ion-nav :root="component" ref="page"></ion-nav>
+</template>
+  
+<script lang="ts">
+  console.log("loginsetup used")
+    import { IonNav } from '@ionic/vue';
+    import SettingsView from './SettingsView.vue';
+  
+    export default {
+      components: { IonNav },
+      data() {
+        return {
+          component: SettingsView,
+        };
+      }
+    };
+</script>
+
+<style scoped>
+    
+</style>
