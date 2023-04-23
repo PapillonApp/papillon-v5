19bd1188ca3406e3240e66396d54999651fa06cd
LeGeek01 <timeobailly02@gmail.com>
2023-04-23 02:55:04 +0200

    modification du router

--- src/router/index.ts
+++ src/router/index.ts
@@ -30,7 +30,7 @@ const routes: Routes = [
   {
     path: '/homework',
     name: 'Homework',
-    component: () => import ('../views/homework/HomeworkView.vue')
+    component: () => import ('../views/homework/HomeworkSetup.vue')
   },
   {
     path: '/homework/hw/:hw?',
