98da4cc2f370fe75083c469a476d5785178c24a9
LeGeek01 <timeobailly02@gmail.com>
2023-05-23 11:35:41 +0200

    add gradesview setup

---
+++ src/views/grades/GradesSetup.vue
@@ -0,0 +1,39 @@
+<template>
+    <ion-page>
+        <ion-nav @ionNavDidChange="navDidChange" @ionNavWillChange="navWillChange" :root="component" ref="page"></ion-nav>
+    </ion-page>
+</template>
+  
+<script lang="ts">
+    import { IonNav, IonPage } from '@ionic/vue';
+    import GradesView from './GradesView.vue';
+
+    //import { StatusBar, Style } from '@capacitor/status-bar';
+
+    //let inner = true;
+  
+    export default {
+      components: { IonNav, IonPage },
+      data() {
+        return {
+          component: GradesView,
+          changeTimeout: null as any
+        };
+      },
+      methods: {
+        navWillChange() {
+          return false;
+        },
+        navDidChange() {
+          return false;
+        }
+      },
+      mounted() {
+        return false;
+      }
+    };
+</script>
+
+<style scoped>
+    
+</style>
